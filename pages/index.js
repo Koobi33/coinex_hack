9d01352f919487184b0f6fad292eab3459fc648b
Ирина Дровянникова <irinadrovannikova@MacBook-Pro-Irina.local>
2022-10-09 11:50:47 +0300

    courses page

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,36 @@
 import styles from "../styles/Home.module.css";
-import { useWeb3React } from "@web3-react/core";
-import { CONTRACT_ABI, CONTRACT_ADDRESS, injected } from "../constants";
-import { useEffect } from "react";
-import { AuthClient } from "../client";
+import {courses} from "../constants/index";
+import Image from "next/image";
+import flame from "../public/flame.svg";
+import cx from 'class-names';
+import Link from 'next/link';
 
 export default function Home() {
-  const { active, account, library, connector, activate, deactivate } =
-    useWeb3React();
-
-  async function connect() {
-    try {
-      await activate(injected);
-    } catch (ex) {
-      console.log(ex);
-    }
-  }
-  //
-  // useEffect(() => {
-  //   console.log({ active, account, library });
-  //   if (active && account && library) {
-  //     signNonce().then((res) => {});
-  //   }
-  //   async function signNonce() {
-  //     const nonce = await AuthClient.getNonce(account);
-  //
-  //     const message = library.utils.sha3(`${nonce}`);
-  //     const signature = await library.eth.personal.sign(
-  //       message,
-  //       account,
-  //       account
-  //     );
-  //
-  //     const token = await AuthClient.signSignature(account, signature);
-  //     localStorage.setItem("token", token.token);
-  //   }
-  // }, [active, account, library]);
-
-  async function disconnect() {
-    try {
-      deactivate();
-    } catch (ex) {
-      console.log(ex);
-    }
-  }
 
   return (
     <div className={styles.container}>
-      {account} - {active}
-      <button
-        onClick={() => {
-          if (account && active) {
-            disconnect();
-          } else {
-            connect();
-          }
-        }}
-      >
-        {active ? "Disconnect" : "Connect"}
-      </button>
+      <p className={styles.page_title}>Courses</p>
+      <div className={styles.courses_container}>
+        {courses?.map(course => (
+            <Link href="/courses/1">
+              <a className={styles.course_container} key={course.id}>
+                <div className={styles.rating_container}>
+                    <Image src={flame}/>
+                    <p className={styles.rating}>4.9</p>
+                    <p className={styles.rating_count}>1 371 ratings</p>
+                </div>
+                <div className={styles.course_title_container}><p className={styles.course_title}>{course.title}</p></div>
+                <div className={cx(styles.course_title_container, styles.course_desc_container)}>
+                  <p className={styles.course_desc}>{course.description}</p>
+                </div>
+                <div className={styles.info_container}>
+                  <p className={styles.info}>{course.grade}</p>
+                  <p className={styles.info}>{course.duration}</p>
+                </div>
+              </a>
+            </Link>
+        ))}
+      </div>
     </div>
   );
 }
